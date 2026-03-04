00e9ccb5af8a4ad865050378e641144345f014dd
stale <leonk.poczta@gmail.com>
2026-03-04 21:54:10 +0100

    Initial commit

---
+++ src/main/java/net/leon/testmod/TestModClient.java
@@ -0,0 +1,10 @@
+package net.leon.testmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TestModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
