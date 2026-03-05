fdbe78d5bc8db68436627af9c6243c1598d6ae2d
stale <leonk.poczta@gmail.com>
2026-03-05 18:59:20 +0100

    added raw and ingot versions of lead, nickel, tin, lithium and tungsten

--- src/main/java/net/leon/testmod/item/ModItems.java
+++ src/main/java/net/leon/testmod/item/ModItems.java
@@ -9,7 +9,16 @@ import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
+    public static final Item TIN_INGOT = registerItem("tin_ingot", new Item(new Item.Settings()));
     public static final Item RAW_TIN = registerItem("raw_tin", new Item(new Item.Settings()));
+    public static final Item NICKEL_INGOT = registerItem("nickel_ingot", new Item(new Item.Settings()));
+    public static final Item RAW_NICKEL = registerItem("raw_nickel", new Item(new Item.Settings()));
+    public static final Item LITHIUM_INGOT = registerItem("lithium_ingot", new Item(new Item.Settings()));
+    public static final Item RAW_LITHIUM = registerItem("raw_lithium", new Item(new Item.Settings()));
+    public static final Item LEAD_INGOT = registerItem("lead_ingot", new Item(new Item.Settings()));
+    public static final Item RAW_LEAD = registerItem("raw_lead", new Item(new Item.Settings()));
+    public static final Item TUNGSTEN_INGOT = registerItem("tungsten_ingot", new Item(new Item.Settings()));
+    public static final Item RAW_TUNGSTEN = registerItem("raw_tungsten", new Item(new Item.Settings()));
 
 
     private static Item registerItem(String name, Item item) {
@@ -21,7 +30,16 @@ public class ModItems {
 
 
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(TIN_INGOT);
             fabricItemGroupEntries.add(RAW_TIN);
+            fabricItemGroupEntries.add(NICKEL_INGOT);
+            fabricItemGroupEntries.add(RAW_NICKEL);
+            fabricItemGroupEntries.add(LITHIUM_INGOT);
+            fabricItemGroupEntries.add(RAW_LITHIUM);
+            fabricItemGroupEntries.add(LEAD_INGOT);
+            fabricItemGroupEntries.add(RAW_LEAD);
+            fabricItemGroupEntries.add(TUNGSTEN_INGOT);
+            fabricItemGroupEntries.add(RAW_TUNGSTEN);
         });
     }
 }
